9f8cbc75531e63f8167c3057940df014f0223735
Kshitij Gupta <gkshitij66@gmail.com>
2026-02-23 17:12:39 +0530

    Time: 1 ms (99.61%), Space: 48.8 MB (87.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,17 +1,21 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int v = nums1.length + nums2.length;
-        int [] arr =  new int [v];
+        //OPTIMAL SOLUTION
         int n = nums1.length;
-        
-        for(int i=0; i<nums1.length; i++){
-            arr[i] = nums1[i];
+        int m = + nums2.length;
+        int v = n+m;
+        int [] arr =  new int [v];
+        int i=0,j=0,k=0;
+
+        while(i<n && j<m){
+            if(nums1[i] < nums2[j]){
+                arr[k++] = nums1[i++];
+            }else{
+                arr[k++] = nums2[j++];
+            }
         }
-        for(int i=0; i<nums2.length; i++){
-            arr[n] = nums2[i];
-            n++;
-        }
-        Arrays.sort(arr);
+        while(i<n) arr[k++] = nums1[i++];
+        while(j<m) arr[k++] = nums2[j++];
         //median
         if(v % 2 ==0){
             double ans = (arr[v/2] + arr[v/2-1]);
@@ -20,6 +24,30 @@ class Solution {
         else{
             double ans = arr[v/2];
             return ans;
-    }
+        }
+
+        //MY SOLUTION
+
+        // int v = nums1.length + nums2.length;
+        // int [] arr =  new int [v];
+        // int n = nums1.length;
+        
+        // for(int i=0; i<nums1.length; i++){
+        //     arr[i] = nums1[i];
+        // }
+        // for(int i=0; i<nums2.length; i++){
+        //     arr[n] = nums2[i];
+        //     n++;
+        // }
+        // Arrays.sort(arr);
+        // //median
+        // if(v % 2 ==0){
+        //     double ans = (arr[v/2] + arr[v/2-1]);
+        //     return ans/2;
+        // }
+        // else{
+        //     double ans = arr[v/2];
+        //     return ans;
+        // }
     }
 }
