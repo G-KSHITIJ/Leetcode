ab301abd750a26a3595e89838df6ee9c85d4df3c
Kshitij Gupta <gkshitij66@gmail.com>
2026-02-25 19:29:19 +0530

    Time: 74 ms (17.83%), Space: 105.8 MB (35.66%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.java
@@ -1,23 +1,46 @@
 class Solution {
     public int countDistinctIntegers(int[] nums) {
-        int n = nums.length;
-        int arr [] = new int[n+n];
-        for(int i=0; i<n; i++){
-            int x = nums[i];
-            int rev = 0;
-            while(x != 0){
-                int lastdigit = x % 10;
-                rev = rev * 10 + lastdigit; 
-                x/=10;
-            }
-            arr[i] = nums[i];
-            arr[n+i] = rev;
+        //OPTIMIZED CODE
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+            set.add(reverse(num));
         }
-        Set<Integer> val = new HashSet<>(); 
-        for(int k : arr){
-            val.add(k);
-        }
-        int ans = val.size();
-        return ans;
+
+        return set.size();
     }
-}
+
+
+    private int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+        return rev;
+    }
+} 
+
+        // MY CODE
+        // int n = nums.length;
+        // int arr [] = new int[n+n];
+        // for(int i=0; i<n; i++){
+        //     int x = nums[i];
+        //     int rev = 0;
+        //     while(x != 0){
+        //         int lastdigit = x % 10;
+        //         rev = rev * 10 + lastdigit; 
+        //         x/=10;
+        //     }
+        //     arr[i] = nums[i];
+        //     arr[n+i] = rev;
+        // }
+        // Set<Integer> val = new HashSet<>(); 
+        // for(int k : arr){
+        //     val.add(k);
+        // }
+        // int ans = val.size();
+        // return ans;
+//     }
+// }
