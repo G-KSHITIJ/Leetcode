47650486d49404533e927460efc931cad7b063d0
Kshitij Gupta <gkshitij66@gmail.com>
2026-03-21 14:41:27 +0530

    Time: 0 ms (100%), Space: 43 MB (52.85%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int count = 0;
+        int ans = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                count++;
+            }
+            else{
+                if(count > 0){
+                    count--;
+                }
+                else ans++;
+            }
+        }
+        return ans+count;
+    }
+}
