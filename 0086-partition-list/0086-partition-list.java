18998c145522483961f66788c15bba460a364a52
Kshitij Gupta <gkshitij66@gmail.com>
2026-07-13 18:37:10 +0530

    Time: 0 ms (100%), Space: 44.1 MB (14.59%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -10,24 +10,50 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode dummy = new ListNode(0); 
-        ListNode ans = dummy;
+        // OPTIMIZED SOL
+        ListNode lessHead = new ListNode(0);
+        ListNode greaterHead = new ListNode(0);
+
         ListNode curr = head;
+        ListNode less = lessHead;
+        ListNode great = greaterHead;
+
         while(curr != null){
             if(curr.val < x){
-                ans.next = new ListNode(curr.val);
-                ans = ans.next;
+                less.next = curr;
+                less = less.next; 
+            }
+            else{
+                great.next = curr;
+                great = great.next;
             }
             curr = curr.next;
         }
-        ListNode prev = head;
-        while(prev != null){
-            if(prev.val >= x){
-                ans.next = new ListNode(prev.val);
-                ans = ans.next;
-            }
-            prev = prev.next;
-        }
-        return dummy.next;
+        great.next = null;
+        less.next = greaterHead.next;
+        return lessHead.next;
     }
 }
+
+
+        // MY CODE
+
+        // ListNode dummy = new ListNode(0); 
+        // ListNode ans = dummy;
+        // ListNode curr = head;
+        // while(curr != null){
+        //     if(curr.val < x){
+        //         ans.next = new ListNode(curr.val);
+        //         ans = ans.next;
+        //     }
+        //     curr = curr.next;
+        // }
+        // ListNode prev = head;
+        // while(prev != null){
+        //     if(prev.val >= x){
+        //         ans.next = new ListNode(prev.val);
+        //         ans = ans.next;
+        //     }
+        //     prev = prev.next;
+        // }
+        // return dummy.next;
